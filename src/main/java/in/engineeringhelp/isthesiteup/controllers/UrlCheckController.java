b1829ae06f6ac2054d34d223b245b15628b3fc1f
Vikas Yadav <v.ray96610@gmail.com>
2022-03-15 21:45:14 +0530

    init

---
+++ src/main/java/in/engineeringhelp/isthesiteup/controllers/UrlCheckController.java
@@ -0,0 +1,37 @@
+package in.engineeringhelp.isthesiteup.controllers;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UrlCheckController {
+
+    private final String SITE_UP = "Site is up!";
+    private final String SITE_DOWN = "Site is down!";
+    private final String INCORRECT_URL = "URL is incorrect";
+
+
+    @GetMapping("/check")
+    public String getUrlStatusMessage(@RequestParam String url){
+        String returnMessage = "";
+
+        try {
+            URL urlObj = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+        } catch (MalformedURLException e) {
+            returnMessage = INCORRECT_URL;
+        } catch (IOException e) {
+            returnMessage = SITE_DOWN;
+        }
+
+
+        return returnMessage;
+    }
+    
+}
